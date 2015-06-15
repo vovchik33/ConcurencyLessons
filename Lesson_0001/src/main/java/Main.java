d07e6f9516b1be3f943df4e6196d6e920e3f0bab
Vladimir Kravchenko <Vladimir.Kravchenko@playtech.com>
2015-06-15 16:18:55 +0300

    Lesson_0001 is completed

--- Lesson_0001/src/main/java/Main.java
+++ Lesson_0001/src/main/java/Main.java
@@ -4,15 +4,30 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Lesson_0001");
-        Thread thread = new Thread(new NewThread());
-        thread.run();
+        for (int i=0; i<5; i++) {
+            Thread thread = new Thread(new NewThread(i));
+            thread.start();
+        }
     }
 
     static class NewThread implements Runnable {
 
+        private int j;
+
+        public NewThread(int i) {
+            j=i;
+        }
+
         @Override
         public void run() {
-            System.out.println("NewThread");
+            for (int i=0; i<2; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(j);
+            }
         }
     }
 }
